206e102379515c6171a970a1191d42c5ec897a7e
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-08-13 18:48:08 +0530

    Create remove-duplicates.cpp

---
+++ remove-duplicates.cpp
@@ -0,0 +1,16 @@
+//Q.- Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int k = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
