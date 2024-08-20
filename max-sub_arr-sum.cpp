29acb85ae5bb993ad75d4bb6462802aee6b2e89e
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-08-20 21:59:59 +0530

    Create max-sub_arr-sum.cpp

---
+++ max-sub_arr-sum.cpp
@@ -0,0 +1,20 @@
+//Q.- Given an integer array nums, find the subarray with the largest sum, and return its sum.
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        long long maxi = LONG_MIN; // maximum sum
+        long long sum = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
