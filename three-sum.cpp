f4996f253a7b7fc498371e74f4dfd591c2128887
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-08-28 23:52:26 +0530

    Update three-sum.cpp

--- three-sum.cpp
+++ three-sum.cpp
@@ -1,6 +1,8 @@
 //Q.- Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 // Notice that the solution set must not contain duplicate triplets
 
+//Used a two pointer approach where I sorted the vector first and then constantly check if the sum is lesser or greater than our requirement 
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
