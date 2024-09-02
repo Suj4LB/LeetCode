08ed5e1810763a9caa8ba71c5d3cb7dad9e973a4
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-09-02 22:49:15 +0530

    Update roman-to-int.cpp

--- roman-to-int.cpp
+++ roman-to-int.cpp
@@ -14,3 +14,23 @@
 // X can be placed before L (50) and C (100) to make 40 and 90. 
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        vector<int> roman(128);
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+        for (int i = 0; i < s.length()-1; ++i){
+            if (roman[s[i]] < roman[s[i + 1]]) ans -= roman[s[i]];
+            else ans += roman[s[i]];
+        }
+        return ans + roman[s.back()];
+    }
+};
