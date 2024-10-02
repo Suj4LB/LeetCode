a58017b81fb471b8ab83df350af3dbecfb63cd7f
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-10-02 09:07:35 +0530

    solution

--- remove_node.java
+++ remove_node.java
@@ -10,3 +10,23 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode temp = new ListNode(0,head);
+        ListNode dummy = temp;
+
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
+ 
+        dummy.next = dummy.next.next;
+        return temp.next;
+    }
+}
