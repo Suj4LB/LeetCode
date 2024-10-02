39fc6867d81f1634b50c88a0da4dbf851c2b23fa
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-10-02 09:05:53 +0530

    sssssssss

---
+++ remove_node.java
@@ -0,0 +1,12 @@
+//Q. - Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
