2a75e5573cfe4bc0d4a586daaa9f2e1fbb634cb8
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-10-02 09:10:06 +0530

    solution

--- merge_sorted_ll.java
+++ merge_sorted_ll.java
@@ -9,3 +9,27 @@
  *     struct ListNode *next;
  * };
  */
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode dum;
+    struct ListNode *temp = &dum;
+    dum.next = NULL;
+    while(list1!=NULL && list2!=NULL){
+        if(list1->val<=list2->val){
+            temp->next = list1;
+            list1 = list1->next;
+        }
+        else{
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        temp = temp->next;
+    }
+    if(list1!=NULL){
+        temp->next = list1;
+    }
+    else{
+        temp->next = list2;
+    }
+    return dum.next; 
+}
