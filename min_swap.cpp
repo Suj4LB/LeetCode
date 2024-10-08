01f0a6de11dc0949bed0f03bf410b3a9d8b9cf4f
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-10-08 22:41:28 +0530

    Create min_swap.cpp

---
+++ min_swap.cpp
@@ -0,0 +1,24 @@
+//Q - You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+// A string is called balanced if and only if:
+// It is the empty string, or
+// It can be written as AB, where both A and B are balanced strings, or
+// It can be written as [C], where C is a balanced string.
+// You may swap the brackets at any two indices any number of times.
+// Return the minimum number of swaps to make s balanced.
+
+//Solution - 
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        int cnt = 0;   
+        for(auto &it : s) {
+            if(it == ']') {
+                if(cnt == 0) cnt++;
+                else cnt--;
+            }
+            else cnt++;
+        }
+        return(cnt + 1)/2;
+    }
+};
